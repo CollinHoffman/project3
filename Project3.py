708ea9c522d85000d85ee514d8d1337eb0bf43e9
CollinHoffman <choff1228@yahoo.com>
2018-02-28 13:12:47 -0600

    testing fixes

--- Project3.py
+++ Project3.py
@@ -6,6 +6,7 @@ import os
 logurl = "https://s3.amazonaws.com/tcmg476/http_access_log" 
 logfile = "locallog.log"
 
+
 #regex for later use ".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*"
 
 #downloads file if file is not already downloaded
@@ -13,18 +14,18 @@ def getFile():
     if not os.path.exists(logfile):
         print("Downloading log file. Please wait.")
         urllib.request.urlretrieve(logurl, "locallog.log")
-    #if file exists, the file is opened.
     else:
         file=open("locallog.log")
 
-
+#parses through file
 def parseFile():
+    print("placeholder")
 
-
+#main method
 def main():
     getFile()
     parseFile()
 
-
+#runs program
 if __name__ == "__main__":
     main()
