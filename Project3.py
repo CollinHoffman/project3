9c7028637d1faa7af48366d2396fdafa80937296
CollinHoffman <choff1228@yahoo.com>
2018-02-28 14:35:18 -0600

    Added status code counter

--- Project3.py
+++ Project3.py
@@ -23,15 +23,27 @@ def parseFile():
     errors = []    
     monthcount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0} #months of the year starting with january at 1
     daycount = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0} #days of the week starting with 0 as monday
+    failcount = 0
+    redirectcount = 0
+    successcount = 0
 
     for line in openlog:
         totalrequests += 1
         lineparts = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
         if len(lineparts) >= 7:
             #regex worked
+            #day and month counter
             dt = datetime.strptime(lineparts[1], "%d/%b/%Y")
             daycount[dt.weekday()] += 1
             monthcount[dt.month] += 1
+
+            #code counters
+            if linparts[7] == '200':
+                successcount += 1
+            elif lineparts[7] == '300':
+                redirectcount += 1
+            else:
+                failcount += 1
         else:
             #regex did not work
             errorcount += 1
@@ -40,12 +52,13 @@ def parseFile():
     #print the amount of logs for each day
     for d in daycount:
         print("There were ", daycount[d], " requests on the day ", d, " over the period represented.")
-        
+
     #print the amount of logs for each month
     for m in monthcount:
         print("There were ", monthcount[m], " requests on the month ", m, " over the period represented.")
 
     print("Over the time period represented in the log there were ", totalrequests, "requests.")
+    print("There were ", successcount, " successful request, ", failcount, " failed requests, and ", redirectcount, " redirected requests.")
 
 
 #main method
