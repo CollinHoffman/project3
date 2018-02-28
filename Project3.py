076d8a06bafd0b63f65bf64d670eaf87ce0e582c
CollinHoffman <choff1228@yahoo.com>
2018-02-28 13:51:01 -0600

    parsing code

--- Project3.py
+++ Project3.py
@@ -7,7 +7,6 @@ import os
 logurl = "https://s3.amazonaws.com/tcmg476/http_access_log" 
 logfile = "locallog.txt"
 
-
 #downloads file if file is not already downloaded
 def getFile():
     if not os.path.exists(logfile):
@@ -21,11 +20,21 @@ def parseFile():
     openlog = open("locallog.txt")
     
     totalrequests = 0
-    errors = []
+    errorcount = 0
+    errors = []    
+    monthcount = {"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0, "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0, "Nov": 0, "Dec": 0}
 
     for line in openlog:
         totalrequests += 1
-        #lineparts = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", openlog)
+        lineparts = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
+        if len(lineparts) >= 7:
+            #regex worked
+            print(lineparts)
+        else:
+            #regex did not work
+            errorcount += 1
+            print("Error parsing line: Line added to errors list")
+            errors.append(line)
     
     print("Over the time period represented in the log there were ", totalrequests, "requests.")
 
