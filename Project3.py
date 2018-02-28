500391f30f8fa5a25156312776b94187c06d02a5
CollinHoffman <choff1228@yahoo.com>
2018-02-28 13:19:29 -0600

    created loop to look through file

--- Project3.py
+++ Project3.py
@@ -6,7 +6,6 @@ import os
 logurl = "https://s3.amazonaws.com/tcmg476/http_access_log" 
 logfile = "locallog.log"
 
-
 #regex for later use ".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*"
 
 #downloads file if file is not already downloaded
@@ -15,11 +14,18 @@ def getFile():
         print("Downloading log file. Please wait.")
         urllib.request.urlretrieve(logurl, "locallog.log")
     else:
-        file=open("locallog.log")
+        print("File is downloaded, parsing now.")
 
 #parses through file
 def parseFile():
-    print("placeholder")
+    openlog = open('locallog.log', 'w')
+    
+    totalrequests = 0
+    errorcount = 0
+
+    for line in openlog:
+        errorcount += 1
+        
 
 #main method
 def main():
