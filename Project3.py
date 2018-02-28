2c6acd5593a49f72b45128393b4563f92cfd55b4
Collin Hoffman <choff1228@yahoo.com>
2018-02-28 12:18:31 -0600

    initial python file created

---
+++ Project3.py
@@ -0,0 +1,17 @@
+import datetime
+from datetime import date, time, timedelta
+import time
+import calendar
+import os
+from os import path
+import shutil
+#import urllib.request
+#import html.parser import HTMLParser
+
+
+def main():
+    i = 1
+
+
+if __name__ == "__main__":
+    main()
