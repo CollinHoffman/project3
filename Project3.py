be06c7f3922b4796f8c0947e1717a9e581feaf4b
CollinHoffman <choff1228@yahoo.com>
2018-02-28 13:30:33 -0600

    changed .log file to .txt

--- Project3.py
+++ Project3.py
@@ -5,20 +5,20 @@ import re
 import os
 
 logurl = "https://s3.amazonaws.com/tcmg476/http_access_log" 
-logfile = "locallog.log"
+logfile = "locallog.txt"
 
 
 #downloads file if file is not already downloaded
 def getFile():
     if not os.path.exists(logfile):
         print("Downloading log file. Please wait.")
-        urllib.request.urlretrieve(logurl, "locallog.log")
+        urllib.request.urlretrieve(logurl, "locallog.txt")
     else:
         print("File is downloaded, parsing now.")
 
 #parses through file
 def parseFile():
-    openlog = open("locallog.log", "w")
+    openlog = open("locallog.txt", "w")
     
     totalrequests = 0
     errors = []
