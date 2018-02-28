5f504c96661cb55509c50f4a70e5eb5c22d76e33
CollinHoffman <choff1228@yahoo.com>
2018-02-28 14:22:28 -0600

    set up counter for days and months

--- Project3.py
+++ Project3.py
@@ -21,7 +21,8 @@ def parseFile():
     totalrequests = 0
     errorcount = 0
     errors = []    
-    monthcount = {"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0, "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0, "Nov": 0, "Dec": 0}
+    monthcount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0} #months of the year starting with january at 1
+    daycount = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0} #days of the week starting with 0 as monday
 
     for line in openlog:
         totalrequests += 1
@@ -29,12 +30,21 @@ def parseFile():
         if len(lineparts) >= 7:
             #regex worked
             dt = datetime.strptime(lineparts[1], "%d/%b/%Y")
-            print(dt)
+            daycount[dt.weekday()] += 1
+            monthcount[dt.month] += 1
         else:
             #regex did not work
             errorcount += 1
             errors.append(line)
     
+    #print the amount of logs for each day
+    for d in daycount:
+        print("There were ", daycount[d], " requests on the day ", d, " over the period represented.")
+        
+    #print the amount of logs for each month
+    for m in monthcount:
+        print("There were ", monthcount[m], " requests on the month ", m, " over the period represented.")
+
     print("Over the time period represented in the log there were ", totalrequests, "requests.")
 
 
