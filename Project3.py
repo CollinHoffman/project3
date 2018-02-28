9a0a9c66913136a63d3a1a3d8eba15c7bfb7a91d
CollinHoffman <choff1228@yahoo.com>
2018-02-28 12:55:05 -0600

    added url variables and getFile() command

--- Project3.py
+++ Project3.py
@@ -3,11 +3,21 @@ from datetime import date, time, timedelta
 import urllib.request
 import os
 
-#test commit
+logurl = "https://s3.amazonaws.com/tcmg476/http_access_log" 
+logfile = "log.txt"
+
+#downloads file if file is not already downloaded
+def getFile():
+    if not os.path.exists(logfile):
+        print("Downloading log file. Please wait.")
+        urllib.request.urlretrieve(logurl, "log.txt")
+    #if file exists, the file is opened.
+    else:
+        file=open("log.txt")
 
 
 def main():
-    i = 1
+    getFile()
 
 
 if __name__ == "__main__":
