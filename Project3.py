60652467f4217c7d4fb386f8ddb8f93be2c7f400
CollinHoffman <choff1228@yahoo.com>
2018-02-28 14:46:19 -0600

    mostly formatting

--- Project3.py
+++ Project3.py
@@ -26,6 +26,7 @@ def parseFile():
     redirectcount = 0
     successcount = 0
 
+    #splits the next read line into usable parts
     for line in openlog:
         totalrequests += 1
         lineparts = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
@@ -36,7 +37,7 @@ def parseFile():
             daycount[dt.weekday()] += 1
             monthcount[dt.month] += 1
 
-            #code counters
+            #status code counters
             if lineparts[7] == '200':
                 successcount += 1
             elif lineparts[7] == '300':
@@ -48,6 +49,9 @@ def parseFile():
             errorcount += 1
             errors.append(line)
     
+    print("Over the time period represented in the log there were ", totalrequests, "requests and ", round(((errorcount/total)*100),2),"percent of the requests were faulty")
+    print("There were ", successcount, " successful request, ", failcount, " failed requests, and ", redirectcount, " redirected requests.")
+
     #print the amount of logs for each day
     for d in daycount:
         print("There were ", daycount[d], " requests on the day ", d, " over the period represented.")
@@ -56,8 +60,8 @@ def parseFile():
     for m in monthcount:
         print("There were ", monthcount[m], " requests on the month ", m, " over the period represented.")
 
-    print("Over the time period represented in the log there were ", totalrequests, "requests.")
-    print("There were ", successcount, " successful request, ", failcount, " failed requests, and ", redirectcount, " redirected requests.")
+
+    openlog.close()
 
 
 #main method
